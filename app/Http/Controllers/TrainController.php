a2113841eafc8f6a0ef5375642a49e8ddc09ab5c
Simone Fortunato <frtsmn94@gmail.com>
2022-10-03 15:37:57 +0200

    Fine

--- app/Http/Controllers/TrainController.php
+++ app/Http/Controllers/TrainController.php
@@ -8,7 +8,7 @@ use App\Train;
 class TrainController extends Controller
 {
     public function mostraListaTreni (){
-        $train= Train::whereDate('orario_partenza', '2022-10-02')->get();
+        $train= Train::whereDate('orario_partenza', '2022-10-03')->get();
         return view('ListaTreni', compact("train"));
     }
 }
--- database/seeds/Trainseedr.php
+++ database/seeds/Trainseedr.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
-use app\Train;
+use App\Train;
 
 class Trainseedr extends Seeder
 {
@@ -12,16 +12,17 @@ class Trainseedr extends Seeder
      */
     public function run()
     {
-        $Train= new Train();
+        
 
-            $faker = \faker\Factory::create('it_IT');
+        $faker = \Faker\Factory::create('it_IT');
 
         for($i=0; $i<50; $i++){
+            $newTrain= new Train();
             $newTrain->azienda=$faker->word();
             $newTrain->stazione_partenza=$faker->city();
             $newTrain->stazione_arrivo=$faker->city();
             $newTrain->orario_partenza=$faker->dateTimeBetween('-3 days', '+3 days');
-            $newTrain->orario_arrivo=$faker->dateTimeBetween('now', '+3 days')();
+            $newTrain->orario_arrivo=$faker->dateTimeBetween('now', '+3 days');
             $newTrain->codice_treno=$faker->bothify();
             $newTrain->numero_carrozza=$faker->randomDigitNotNull('???-####');
             $newTrain->in_orario=$faker->boolean();
